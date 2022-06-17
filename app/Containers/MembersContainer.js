3168a1575c0376853ec9d2c8b0f5351582999a19
Hugo <hugo.chabert@laplateforme.io>
2022-06-17 15:57:22 +0200

    add Activity Members and Publish containers

--- app/Containers/MembersContainer.js
+++ app/Containers/MembersContainer.js
@@ -4,12 +4,12 @@ import { useUserContext } from '../context/UserContext';
 
 import { colors } from '../utils/colors';
 
-export default AccountContainer = ({ navigation }) => {
+export default MembersContainer = ({ navigation }) => {
 
     const userContext = useUserContext();
 
     React.useEffect(() => {
-        console.log('Account')
+        console.log('Members', userContext)
     }, [])
 
     React.useEffect(() => {
@@ -20,10 +20,7 @@ export default AccountContainer = ({ navigation }) => {
 
     return (
         <SafeAreaView style={{ height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ fontSize: 26, fontWeight: 'bold', color: colors.black }}>Page de compte</Text>
-            <TouchableOpacity onPress={userContext.disconnect} style={{ marginTop: 10 }}>
-                <Text style={{ fontSize: 18, fontWeight: 'normal', color: colors.red }}>Déconnexion</Text>
-            </TouchableOpacity>
+            <Text style={{ fontSize: 26, fontWeight: 'bold', color: colors.black }}>Membres</Text>
         </SafeAreaView>
     );
 }
