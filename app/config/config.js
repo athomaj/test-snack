427c18b2c39678e74dc6e7a70a8714b26b7f3712
Johan <johan.bouguermouh@laplateforme.io>
2022-08-10 11:41:26 +0200

    Config.js in GitIgnor

--- app/config/config.js
+++ app/config/config.js
@@ -1,2 +1,4 @@
 export const BASE_URL = "http://localhost:1337"
 export const API_URL = "http://localhost:1337/api"
+export const BASE_URL = "http://localhost:1337"
+export const API_URL = "http://localhost:1337/api"
